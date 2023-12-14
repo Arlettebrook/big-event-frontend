2a3334fcee3e67cfa459da5341a013b8ee0aa8e2
Arlettebrook <arlettebrook@foxmail.com>
2023-12-14 18:26:25 +0800

    未登录的统一处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,7 +9,6 @@ const baseURL = "/api";
 const instance = axios.create({ baseURL });
 
 import { useTokenStore } from "@/stores/token.js";
-
 // 添加请求拦截器
 instance.interceptors.request.use(
   (config) => {
@@ -28,6 +27,9 @@ instance.interceptors.request.use(
   }
 );
 
+/* import { useRouter } from "vue-router";
+const router = useRouter(); */
+import router from '@/router'
 //添加响应拦截器
 instance.interceptors.response.use(
   (result) => {
@@ -41,8 +43,18 @@ instance.interceptors.response.use(
     return Promise.reject(result.data); //异步的状态转化成失败的状态
   },
   (err) => {
-    // alert(err?err:"服务器异常");
-    ElMessage.error(err ? err : "服务器异常");
+    // 401未登录，跳到登录页面
+    if (err.response.status === 401) {
+      ElMessage.error("请先登录");
+      router.push("/login");
+    } else {
+      // alert(err?err:"服务器异常");
+      
+      ElMessage.error(
+        err.response.data ? err.response.data.message : "服务器异常：" + err
+      );
+    }
+
     return Promise.reject(err); //异步的状态转化成失败的状态
   }
 );
