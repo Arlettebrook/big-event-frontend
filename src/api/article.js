fc0460030777fba6b41ee876bf406ef19f18b639
Arlettebrook <arlettebrook@foxmail.com>
2023-12-14 17:52:07 +0800

    add: 请求拦截器:携带token

--- src/api/article.js
+++ src/api/article.js
@@ -1,10 +1,11 @@
 import request from "@/utils/request.js";
-import { useTokenStore } from "@/stores/token.js";
+// import { useTokenStore } from "@/stores/token.js";
 
 // 文章分类列表查询
 export const articleCategoryListService = () => {
-  const tokenStore = useTokenStore();
-  return request.get("/category", {
+//   const tokenStore = useTokenStore();
+  /* return request.get("/category", {
     headers: { Authorization: tokenStore.token },
-  });
+  }); */
+  return request.get("/category");
 };
--- src/utils/request.js
+++ src/utils/request.js
@@ -8,21 +8,41 @@ import axios from "axios";
 const baseURL = "/api";
 const instance = axios.create({ baseURL });
 
+import { useTokenStore } from "@/stores/token.js";
+
+// 添加请求拦截器
+instance.interceptors.request.use(
+  (config) => {
+    // 请求前的回调
+    // 获取pinia管理的数据
+    const tokenStore = useTokenStore();
+    // 判断有没有token
+    if (tokenStore.token) {
+      config.headers.Authorization = tokenStore.token;
+    }
+    return config;
+  },
+  (err) => {
+    // 错误回调
+    return Promise.reject(err);
+  }
+);
+
 //添加响应拦截器
 instance.interceptors.response.use(
   (result) => {
     // 判断业务状态码
-    if(result.data.code===0){
-      return result.data
+    if (result.data.code === 0) {
+      return result.data;
     }
     // 操作失败
     // alert(result.data.message?result.data.message:"服务器异常")
-    ElMessage.error(result.data.message?result.data.message:"服务器异常")
-    return Promise.reject(result.data)//异步的状态转化成失败的状态
+    ElMessage.error(result.data.message ? result.data.message : "服务器异常");
+    return Promise.reject(result.data); //异步的状态转化成失败的状态
   },
   (err) => {
     // alert(err?err:"服务器异常");
-    ElMessage.error(err?err:"服务器异常")
+    ElMessage.error(err ? err : "服务器异常");
     return Promise.reject(err); //异步的状态转化成失败的状态
   }
 );
